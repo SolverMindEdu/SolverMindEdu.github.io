6dc9e09c60b9588fb230395b6f70e74450f7f906
Ryan Zhang <solver.mind.edu@gmail.com>
2025-10-30 17:19:09 -0400

    attempt fix auton

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -5,11 +5,11 @@ var config_data = `
   "page_title": "REEFSCAPE",
   "checkboxAs": "10",
   "prematch": [
-    { "name": "Scouter Initials",
+    { "name": "Scouter Name",
       "code": "s",
       "type": "scouter",
-      "size": 5,
-      "maxSize": 5,
+      "size": 15,
+      "maxSize": 30,
       "required": "true"
     },
     { "name": "Match Level",
@@ -34,13 +34,21 @@ var config_data = `
       "code": "t",
       "type": "team",
       "min": 1,
-      "max": 99999
+      "max": 99999,
+      "required": "true"
     }
   ],
   "auton": [
-    { "name": "Leave Starting Line",
-      "code": "al",
-      "type": "bool"
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "radio",
+      "choices": {
+        "p": "Close to Processor<br>",
+        "m": "Middle<br>",
+        "fp": "Far from Processor",
+      },
+      "defaultValue": "m"
+      "required": "true"
     },
     { "name": "Coral L1",
       "code": "ac1",
@@ -57,14 +65,6 @@ var config_data = `
     { "name": "Coral L4",
       "code": "ac4",
       "type": "counter"
-    },
-    { "name": "Auto Scoring Position",
-      "code": "asp",
-      "type": "clickable_image",
-      "filename": "2025/reef.png",
-      "dimensions": "6 6",
-      "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
-      "shape": "circle 5 black red true"
     },
     { "name": "Processor Score",
       "code": "aps",
