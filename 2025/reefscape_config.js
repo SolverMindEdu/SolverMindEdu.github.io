50d8eeb3755103a4600857fa2876a08650650f9a
Ryan Kong <ryankong200@gmail.com>
2026-03-13 11:36:43 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -8,7 +8,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026BCVI",
+      "defaultValue": "2026ONOSH",
       "required": "true"
     },
     { "name": "Scouter Name",
